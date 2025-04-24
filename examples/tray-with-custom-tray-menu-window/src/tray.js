c97449d7fc92e267d2232bbbd828af4747dcf685
Adelino Ngomacha <Adelinob335@gmail.com>
2025-04-25 00:10:52 +0200

    feat: add custom windows and call programatically

--- examples/tray-with-custom-tray-menu-window/src/tray.js
+++ examples/tray-with-custom-tray-menu-window/src/tray.js
@@ -3,6 +3,7 @@ import { defaultWindowIcon } from '@tauri-apps/api/app'
 import { TrayIcon } from '@tauri-apps/api/tray'
 import { Webview } from '@tauri-apps/api/webview'
 import { Window } from '@tauri-apps/api/window'
+// import {moveWindow, handleIconState, Position} from '@tauri-apps/plugin-positioner'
 
 /**
  * Initialize the system tray icon and menu
@@ -11,8 +12,18 @@ export async function initTray() {
   try {
     // eslint-disable-next-line no-console
     console.log('Initializing system tray...')
-
     const appWindow = window.getCurrentWindow()
+    const trayWindow = createTrayCustomWindow()
+
+    const action = async (event) => {
+      // await handleIconState(event)
+
+      if (event.type === 'Click') {
+        // eslint-disable-next-line no-console
+        console.log('Tray icon clicked')
+        trayWindow.show()
+      }
+    }
 
     // Create the tray icon with minimal options
     const trayIcon = await TrayIcon.new({
@@ -20,52 +31,7 @@ export async function initTray() {
       icon: await defaultWindowIcon(),
       tooltip: 'Tauri Example App',
       iconAsTemplate: true,
-    })
-
-    const trayKey = 'tray-window'
-    const customTrayWindow = new Window(trayKey, {
-      // parent: appWindow,
-      // center: true,
-      focus: true,
-      decorations: false,
-      resizable: false,
-      height: 300,
-      width: 400,
-      skipTaskbar: true,
-    })
-
-    customTrayWindow.once('tauri://created', () => {
-      // The window was created successfully
-      // eslint-disable-next-line no-console
-      console.log('Custom tray window created successfully')
-
-      const webview = new Webview(customTrayWindow, trayKey, {
-        url: '../index.html',
-        height: 300,
-        width: 400,
-        x: 0,
-        y: 0,
-        devtools: true,
-      })
-
-      webview.once('tauri://created', () => {
-        // The webview was created successfully
-        // eslint-disable-next-line no-console
-        console.log('Webview created successfully')
-        customTrayWindow.show()
-        webview.show()
-      })
-      webview.once('tauri://error', (e) => {
-        // The webview encountered an error
-
-        console.error('Webview error:', e)
-      })
-    })
-
-    customTrayWindow.once('tauri://error', (e) => {
-      // The window encountered an error
-
-      console.error('Custom tray window error:', e)
+      action,
     })
 
     // eslint-disable-next-line no-console
@@ -85,4 +51,53 @@ export async function initTray() {
     console.error('Failed to initialize system tray:', error)
     throw error
   }
+}
+
+function createTrayCustomWindow(url = '../index.html') {
+  const trayKey = 'tray-window'
+  const customTrayWindow = new Window(trayKey, {
+    focus: true,
+    decorations: false,
+    resizable: false,
+    height: 300,
+    width: 400,
+    skipTaskbar: true,
+    visible: false,
+  })
+
+  customTrayWindow.once('tauri://created', () => {
+    // The window was created successfully
+    // eslint-disable-next-line no-console
+    console.log('Custom tray window created successfully')
+
+    const webview = new Webview(customTrayWindow, trayKey, {
+      // url: 'https://github.com/tauri-apps/tauri',
+      url,
+      height: 300,
+      width: 400,
+      x: 0,
+      y: 0,
+    })
+
+    webview.once('tauri://created', () => {
+      // The webview was created successfully
+      // eslint-disable-next-line no-console
+      console.log('Webview created successfully')
+      // customTrayWindow.show()
+      // webview.show()
+    })
+    webview.once('tauri://error', (e) => {
+      // The webview encountered an error
+
+      console.error('Webview error:', e)
+    })
+  })
+
+  customTrayWindow.once('tauri://error', (e) => {
+    // The window encountered an error
+
+    console.error('Custom tray window error:', e)
+  })
+
+  return customTrayWindow
 }
